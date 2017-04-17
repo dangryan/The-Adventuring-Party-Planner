c8b5569eb6346b5601a4b4ec9d9becdff835c520
dangryan <dangryan@iu.edu>
2017-04-16 20:58:38 -0400

    Theme fixes

--- TheAdventuringPartyPlanner/app/src/main/java/theadventuringparty/theadventuringpartyplanner/Main2Activity.java
+++ TheAdventuringPartyPlanner/app/src/main/java/theadventuringparty/theadventuringpartyplanner/Main2Activity.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -46,6 +47,7 @@ public class Main2Activity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
